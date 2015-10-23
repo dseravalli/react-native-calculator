f350fb3e280029d89eb6c3c25a873d4571ef89e6
Dan Seravalli <danielseravalli@gmail.com>
2015-10-23 00:43:12 -0400

    calculator.js

---
+++ calculator.js
@@ -0,0 +1,28 @@
+'use strict';
+
+class Calculator {
+
+  constructor() {
+    this.stack = [];
+  }
+
+  get add() {
+  }
+
+  get subtract() {
+  }
+
+  get multiply() {
+  }
+
+  get divide() {
+  }
+
+  get clear() {
+    this.stack =[];
+  }
+
+  get compute() {
+  }
+
+}
